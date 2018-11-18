f9128bb39ff91dd5d9904b354fb790044a64524d
Daniel Garcia dos Santos <39714005+d4n13lg4rc14@users.noreply.github.com>
2018-11-18 13:57:25 -0500

    Created Basket UI

---
+++ MyShop/MyShop.WebUI/Controllers/BasketController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MyShop.Core.Contracts;
+
+namespace MyShop.WebUI.Controllers
+{
+    public class BasketController : Controller
+    {
+        IBasketServices basketServices;
+
+        public BasketController(IBasketServices BasketService)
+        {
+            this.basketServices = BasketService;
+        }
+        // GET: Basket
+        public ActionResult Index()
+        {
+            var model = basketServices.GetBasketItems(this.HttpContext);
+
+            return View(model);
+        }
+
+        public ActionResult AddToBasket(string Id)
+        {
+            basketServices.AddToBasket(this.HttpContext, Id);
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult RemoveFromBasket(string Id)
+        {
+            basketServices.RemoveFromBasket(this.HttpContext, Id);
+
+            return RedirectToAction("Index");
+        }
+
+        public PartialViewResult BasketSummary()
+        {
+            var basketSummary = basketServices.GetBasketSummary(this.HttpContext);
+
+            return PartialView(basketSummary);
+        }
+    }
+}
